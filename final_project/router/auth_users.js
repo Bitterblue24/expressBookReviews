9222250b173eca3fed6f0a3a9ab6627d13961fe1
Bitterblue24 <taylorguasta24@gmail.com>
2024-09-18 12:54:52 -0400

    Update auth_users.js
    does not work but unfortunately here we are

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,34 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+const user = req.body.username
+if (user.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password)=>{ //returns boolean
+    // Filter the users array for any user with the same username and password
+    let validUsers = users.filter(user => {
+    return user.username === username && user.password === password;
+    });
+        // Return true if any valid user is found, otherwise false
+        return validUsers.length > 0;
+};
+    
+        
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
 });
 
 // Add a book review
